5d08bb8cc82282525682ad243523d6740e502fd9
unknown <jfuerlinger98@gmail.com>
2022-12-24 14:00:07 -0500

    Final version

--- src/am8.cpp
+++ src/am8.cpp
@@ -1,10 +1,41 @@
 #include "am8.hpp"
+#include "main.hpp"
 
 void am8_startup() {
+    leds.fill_solid(CRGB::Black);
+    FastLED.show();    
 
 }
 
+void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
+
 void am8_loop() {
-    leds.fill_solid(CRGB::FireBrick);
-    FastLED.show();
+	static uint8_t hue = 0;
+	Serial.print("x");
+	// First slide the led in one direction
+	for(int i = 0; i < NUM_LEDS; i++) {
+		// Set the i'th led to red 
+		leds[i] = CHSV(hue++, 255, 255);
+		// Show the leds
+		FastLED.show(); 
+		// now that we've shown the leds, reset the i'th led to black
+		// leds[i] = CRGB::Black;
+		fadeall();
+		// Wait a little bit before we loop around and do it again
+		delay(10);
+	}
+	Serial.print("x");
+
+	// Now go in the other direction.  
+	for(int i = (NUM_LEDS)-1; i >= 0; i--) {
+		// Set the i'th led to red 
+		leds[i] = CHSV(hue++, 255, 255);
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		// leds[i] = CRGB::Black;
+		fadeall();
+		// Wait a little bit before we loop around and do it again
+		delay(10);
+	}
 }
